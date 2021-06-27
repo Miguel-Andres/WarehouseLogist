f2c39767961718651fe510d4f55a2765d617824f
Miguel-Andres <silva.andresmiguel@gmail.com>
2021-06-27 15:19:53 -0300

    card template

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,52 @@
-import React from 'react'
-import {Stack} from "@chakra-ui/react"
+import React from "react";
+import { Stack, Container, Text, Button, Heading } from "@chakra-ui/react";
+import {AiOutlineArrowRight } from "react-icons/ai"
+import Count from "./card/Count";
 
 export default function Card() {
-    return (
-        <React.Fragment>
-
-            <Stack m="2px" bg="white" boxShadow="dark-lg" >
-
-                <h2>card</h2>
-
+  return (
+    <React.Fragment>
+      <Container
+        maxW="sm"
+        m="5px"
+        bg="white"
+        p={4}
+        borderWidth="1px"
+        borderRadius="lg"
+      >
+        <Stack>
+          <Heading size="md">Jose Bernardo</Heading>
+          <Stack direction="row" justify="space-around">
+            <Stack>
+              <Text size="sm">Charge</Text>
+              <Count />
             </Stack>
 
-            
-            
-        </React.Fragment>
-    )
+            <Stack>
+              <Text>discharge</Text>
+              <Count />
+            </Stack>
+
+            <Stack align="center">
+              <Heading size="sm"> Duration </Heading>
+              <Text>30 </Text>
+            </Stack>
+          </Stack>
+
+          <Stack direction="row" justify="space-between" spacing={4}>
+            <Button  variant="outline" borderColor="red">
+              Back
+            </Button>
+            <Button
+              rightIcon={<AiOutlineArrowRight />}
+              colorScheme="teal"
+              variant="outline"
+            >
+              Next
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </React.Fragment>
+  );
 }
