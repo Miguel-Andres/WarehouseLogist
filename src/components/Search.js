5c1cf5ac841178f3122e8fa993c91cfe5d217dae
Miguel-Andres <silva.andresmiguel@gmail.com>
2021-07-04 17:21:36 -0300

    input add  ON

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,32 @@
-import React from "react";
-import { Container, Input, InputGroup, InputRightAddon } from "@chakra-ui/react";
+import React, {  useState } from "react";
+import { Container, Input, InputGroup,  Button} from "@chakra-ui/react";
 
 export default function Search() {
 
-   
+    const [input, setInput] = useState("")
+    const [dato ,setDato]=useState("")
+  
+   const handleChange=(e)=>{    
+    setInput(e.target.value)  
+   } 
 
+   const enviar=(e)=>{    
+      e.preventDefault()
+      setDato([...dato,{name:input}])   
+      setInput("")
+   }
+ 
   return (
     <React.Fragment>
       <Container mb={3}>
+
+        <form onSubmit={enviar}>       
         <InputGroup boxShadow="dark-lg">
-          <Input type="text" placeholder="Nombre" bg="white" />
-          <InputRightAddon children="Agregar" />
-        </InputGroup>
+          <Input type="text" placeholder="Nombre" bg="white" onChange={handleChange} name="name" value={input}/>
+          <Button type="submit">Agrega</Button>
+        </InputGroup> 
+        </form>
+        
       </Container>
     </React.Fragment>
   );
