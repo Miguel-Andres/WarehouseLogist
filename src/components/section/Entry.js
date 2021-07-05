9b31506ee9032c77d6fa4658fc54d9224d1139da
Miguel-Andres <silva.andresmiguel@gmail.com>
2021-07-04 23:08:57 -0300

    Seccion process

--- src/components/section/Entry.js
+++ src/components/section/Entry.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react'
+
+import Card from "../Card"
+import {UserContext} from "../../context/UserContext" 
+
+
 
 export default function Entry() {
+    const [user] = useContext(UserContext)
+
+
     return (
-        <div>
-            <h2>Entry</h2>
-        </div>
+       <React.Fragment>          
+       {user.length? user.map(item=>  <Card  name={item.name} key={item.name}/>) : <h2>Cargar Moover</h2>}
+       </React.Fragment>
     )
 }
