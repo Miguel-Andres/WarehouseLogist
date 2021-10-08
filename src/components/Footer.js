b8db045416955efc41411dd5c35bfbd5f35d806b
Miguel-Andres <miguel_3715@hotmail.com>
2021-10-08 17:00:52 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Stack,Text} from "@chakra-ui/react"
 
 export default function Footer() {
     return (
         <React.Fragment>
-            <h2>Footer</h2>
+        <Stack bg="red">
+           <Text >App </Text> 
+        </Stack>
         </React.Fragment>
     )
 }
